05d6a0431baef90483f0a97605d1d755900fcf0c
gondalwad <sudarshangondalwad@gmail.com>
2024-04-22 18:30:24 +0530

    Set of pages like Home, About, SignIn, ........

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// All Pages of the website
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
+// Default Export Component
 export default function App() {
   return (
-    <h1 className='font-bold text-red-600 text-3xl'>
-      Prism Post
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
       
     
   )
