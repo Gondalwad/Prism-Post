91dab75dd593d4662082f5b5d5ea05225355b5e5
gondalwad <sudarshangondalwad@gmail.com>
2024-04-23 13:23:53 +0530

    Database connection using MongoDB cloud

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 const app = express();
 
+//Connecting to database
+dotenv.config();
+mongoose.connect(process.env.MONGOURL).then(()=>{
+    console.log("Connected to Database");
+}).catch((err)=>{
+    console.log(err);
+});
 
 app.listen(3000,()=>{
     console.log("The server is running on port 3000");
