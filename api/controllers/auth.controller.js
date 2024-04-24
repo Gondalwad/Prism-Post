602cbe3a1df7e0e3ac0ce5dc1a310a4c72948a64
gondalwad <sudarshangondalwad@gmail.com>
2024-04-24 16:50:00 +0530

    Created Sign IN api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,11 @@
 import user from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 export const signup = async (req, res, next) => {
     const username = req.body.username.trim();
@@ -8,7 +13,7 @@ export const signup = async (req, res, next) => {
     const password = req.body.password.trim();
 
     if (!username || !email || !password || username === '' || password === '' || email === '') {
-        next(errorHandler(400,"All fields are required"));
+        next(errorHandler(400, "All fields are required"));
     }
 
     const hashedpassword = bcryptjs.hashSync(password, 10);
@@ -22,7 +27,43 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.json("Account Created Successfull");
-    }catch(err){
+    } catch (err) {
         next(err);
+    }
+}
+
+
+//Signin Function 
+export const signin = async (req, res, next) => {
+    const { username, password } = req.body;
+
+    if (!username || !password || username === '' || password === '') {
+        return next(errorHandler(400, "Enter Valid Credentials"));
+    }
+
+    // finding user
+    try {
+
+
+        const validUser = await User.findOne({ username });
+        if (!validUser) {
+            return next(errorHandler(404, "Invalid Username or Password"));
+        }
+        // checking password
+        const validPass = await bcryptjs.compareSync(password, validUser.password);
+        if (!validPass) {
+            return next(errorHandler(404, "Invalid Username or Password"));
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        const { password: pass, ...rest } = validUser._doc;
+
+
+        return res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json(rest);
+    } catch (error) {
+        return next(error);
     }
 }
