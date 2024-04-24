1b8d7eb7233639818fd4ab5d3df117f509900ec8
gondalwad <sudarshangondalwad@gmail.com>
2024-04-25 01:36:39 +0530

    Sign In UI and Signin user - 1 but after reload it vanishes

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,9 +1,127 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Link,useNavigate } from 'react-router-dom'
+import Logo from '../components/Logo'
 
+// Sign Up function
 export default function SignIn() {
+  // userstate
+  let [formdata, setFormdata] = useState({});
+  let [errormessage, setErrorMessage] = useState(null);
+  let [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  // function to handle and target formdata 
+  function dataCollector(e) {
+    setFormdata({ ...formdata, [e.target.id]: e.target.value.trim() })
+  }
+
+  // Function to handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    setErrorMessage(null); // setting value to default to null when function is called
+    setLoading(true);
+
+    // Showing error for invalid inputs
+    if (!formdata.username || !formdata.password) {
+      setLoading(false);
+      return setErrorMessage("All Fields are Mandatory")
+    }
+
+    try {
+      const res = await fetch('/api/user/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formdata)
+      });
+
+      const data = await res.json();
+      // if error from serverside
+      if (data.success === false) {
+        // console.log(data);
+        setLoading(false);
+        console.log(data);
+        return setErrorMessage(data.message);
+      }
+
+      setLoading(false);
+      if(res.ok){
+        navigate('/');
+      }
+
+    } catch (err) {
+      setErrorMessage(err);
+      setLoading(false);
+    }
+  }
+
+
+
+
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-3' >
+
+        {/*######################## left ########################*/}
+        <div className='flex-1'>
+          <Link to='/' className='text-3xl font-bold  '>
+            <Logo />
+          </Link>
+          <p className='text-sm mt-5'>
+            "Sign In Prism"
+          </p>
+        </div>
+        {/*######################## Right ########################*/} {/*       form           */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
+            {/* ((((((( Username ))))))) */}
+            <div>
+              <Label className='' value='Your Username' />
+              <TextInput
+                type='text'
+                placeholder='Username'
+                id='username'
+                onChange={dataCollector}
+              />
+            </div>
+          
+            {/* ((((((((( Password ))))))))) */}
+            <div>
+              <Label className='' value='Your Password' />
+              <TextInput
+                type='password'
+                placeholder='********'
+                id='password'
+                onChange={dataCollector}
+              />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit' outline disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm' />
+                    <span>Signing In...</span>
+                  </>
+                ) : 'Sign In'
+              }
+            </Button>
+          </form>
+          {/* ############## Option to Sign IN ############## */}
+          <div className='mt-2 text-sm'>
+            <span>Don't Have an Account ?</span>
+            <Link to='/sign-up' className='ml-2 text-blue-500'>
+              Sign Up
+            </Link>
+          </div>
+          {
+            errormessage && (
+              <Alert className='mt-5' color='failure'>
+                {errormessage}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
     </div>
   )
 }
