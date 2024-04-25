b0a13cc75b16bb1776e3eda150cc8a2cbe9c0818
gondalwad <sudarshangondalwad@gmail.com>
2024-04-25 19:22:22 +0530

    completed SideBar of profile

---
+++ Client/src/components/DashboardSideBar.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import {Sidebar} from  'flowbite-react' 
+import {HiArrowSmRight, HiUser} from 'react-icons/hi'
+import { Link } from 'react-router-dom'
+
+export default function DashboardSideBar() {
+    const location = useLocation();
+
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const tabFromUrl = urlParams.get('tab');
+      if(tabFromUrl){
+        setTab(tabFromUrl);
+      }
+    }, [location.search]);
+  return (
+    <Sidebar className='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to='/dashboard?tab=profile'>
+                <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'user'} lablecolor='dark'>
+                    Profile
+                </Sidebar.Item> 
+                </Link>
+                <Sidebar.Item icon={HiArrowSmRight}>
+                    Sign Out
+                </Sidebar.Item> 
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { get } from 'mongoose';
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashboardProfile from '../components/DashboardProfile'
+import DashboardSideBar from '../components/DashboardSideBar'
 
 export default function Dashboard() {
+  const location = useLocation();
+
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+ 
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      {/* sidebar */}
+      <div className='md:min-w-56'>
+
+      <DashboardSideBar/>
+      </div>
+
+      {/* Profileler ... etc */}
+
+      {tab === 'profile' && <DashboardProfile/>}
     </div>
   )
 }
